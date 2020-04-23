8ed8b06d1eb32d2d87d8c39aa519fd9dc83a8ef2
Christian Francia <cfrancia@Christians-MBP.home>
2020-04-23 12:26:44 +0200

    init

---
+++ christianmath/math.go
@@ -0,0 +1,7 @@
+// Package christianmath is just a test for stuff.
+package christianmath
+
+// Sum takes two integers, adds them together and returns int
+func Sum(i1 int, i2 int) int {
+	return i1 + i2
+}
